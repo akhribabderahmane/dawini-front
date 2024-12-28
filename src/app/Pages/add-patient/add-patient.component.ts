b9a873aae771c5a274b574559e764de200d10383
unknown <amirabenatmane01@gmail.com>
2024-12-29 00:32:57 +0100

    feat: add Add New Patient page

---
+++ src/app/Pages/add-patient/add-patient.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NewPatientComponent } from '../../components/new-patient/new-patient.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { HeaderProfileComponent } from '../../components/header-profile/header-profile.component';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-add-patient',
+  imports: [NewPatientComponent,HeaderProfileComponent,SidebarComponent,RouterModule ],
+  templateUrl: './add-patient.component.html',
+  styleUrl: './add-patient.component.css'
+})
+export class AddPatientComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,20 @@ import { AppointmentsComponent } from './Pages/appointments/appointments.compone
 import { HomeComponent } from './Pages/home/home.component';
 import { AppointmentsDetailsComponent } from './Pages/appointments-details/appointments-details.component';
 import { PatientProfileComponent } from './Pages/patient-profile/patient-profile.component';
+import { ModifyPatientComponent } from './Pages/modify-patient/modify-patient.component';
+import { AddPatientComponent } from './Pages/add-patient/add-patient.component';
 export const routes: Routes = [
   { path: 'appointments', component: AppointmentsComponent },
   //{ path: 'profile', component: ProfilePageComponent },
   { path: 'login', component:HomeComponent},
-  { path: '', redirectTo: '/appointments', pathMatch: 'full' }, // Default redirection
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Default redirection
   { path: 'appointment-details/:id', component: AppointmentsDetailsComponent },
   { path: 'profile/:id', component: PatientProfileComponent },
+  {path: 'new-patient',component: AddPatientComponent},
+  {
+    path: 'edit-patient/:id',
+    component:ModifyPatientComponent,
+  }
 ];
 
 export const AppRoutingModule = provideRouter(routes);

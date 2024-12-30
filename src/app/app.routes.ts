2dbdac2174d2c24b39ca904a1cd7d2494bbaeff9
Islam Tobni <islamtobni@gmail.com>
2024-12-30 05:16:09 +0100

    checked login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,17 @@
 
 import { provideRouter, Routes } from '@angular/router';
-import { AppointmentsComponent } from './Pages/appointments/appointments.component';
+//import { AppointmentsComponent } from './Pages/appointments/appointments.component';
 //import { ProfilePageComponent } from './pages/profile/profile-page.component';
 import { HomeComponent } from './Pages/home/home.component';
-import { AppointmentsDetailsComponent } from './Pages/appointments-details/appointments-details.component';
-import { PatientProfileComponent } from './Pages/patient-profile/patient-profile.component';
+//import { AppointmentsDetailsComponent } from './Pages/appointments-details/appointments-details.component';
+//import { PatientProfileComponent } from './Pages/patient-profile/patient-profile.component';
 export const routes: Routes = [
-  { path: 'appointments', component: AppointmentsComponent },
+  //{ path: 'appointments', component: AppointmentsComponent },
   //{ path: 'profile', component: ProfilePageComponent },
   { path: 'login', component:HomeComponent},
   { path: '', redirectTo: '/appointments', pathMatch: 'full' }, // Default redirection
-  { path: 'appointment-details/:id', component: AppointmentsDetailsComponent },
-  { path: 'profile/:id', component: PatientProfileComponent },
+  //{ path: 'appointment-details/:id', component: AppointmentsDetailsComponent },
+  //{ path: 'profile/:id', component: PatientProfileComponent },
 ];
 
 export const AppRoutingModule = provideRouter(routes);

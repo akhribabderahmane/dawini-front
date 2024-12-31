94a763135cdf34763459619272acc4a94521e06d
unknown <amirabenatmane01@gmail.com>
2024-12-31 21:36:31 +0100

    WIP: Save progress in app.routes.ts and login.component.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ import { AppointmentsDetailsComponent } from './Pages/appointments-details/appoi
 import { PatientProfileComponent } from './Pages/patient-profile/patient-profile.component';
 import { ModifyPatientComponent } from './Pages/modify-patient/modify-patient.component';
 import { AddPatientComponent } from './Pages/add-patient/add-patient.component';
+import {DoctorProfileComponent} from './Pages/doctor-profile/doctor-profile.component';
+import { ListeDesPatientsComponent} from './Pages/liste-des-patients/liste-des-patients.component'
+import { PatientAppointmentsComponent } from './Pages/patient-appointments/patient-appointments.component';
+import {CreateConsultationComponent} from './Pages/create-consultation/create-consultation.component';
 export const routes: Routes = [
   { path: 'appointments', component: AppointmentsComponent },
   //{ path: 'profile', component: ProfilePageComponent },
@@ -17,7 +21,12 @@ export const routes: Routes = [
   {
     path: 'edit-patient/:id',
     component:ModifyPatientComponent,
-  }
+  },
+  { path: 'doctor-profile', component: DoctorProfileComponent },
+  {path:'patientlist',component:ListeDesPatientsComponent},
+  {path:'PatientAppointments/:id',component:PatientAppointmentsComponent},
+  {path:'add-appointment/:id',component:CreateConsultationComponent}
+
 ];
 
 export const AppRoutingModule = provideRouter(routes);
